9361cacac2656126980feff375129d455ddd6e4a
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-05-28 23:01:57 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ mkdir ~/.config/polybar/
 cp -i ~/wm/config.ini ~/.config/polybar/config.ini
 #Qtile
 cp -i ~/wm/config.py ~/.config/qtile/config.py
-chmod +x ~/Downloads/wm/autostart.sh
+chmod +x ~/wm/autostart.sh
 cp -i ~/wm/autostart.sh ~/.config/qtile/autostart.sh
 #Wallpaper
 cp -i ~/wm/image.jpg ~/.config/qtile/image.jpg
