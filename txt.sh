c97f10e44275351c778e74b637ab4c2101aa6dc8
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-05-31 20:41:21 +0200

    Add files via upload

---
+++ txt.sh
@@ -0,0 +1,70 @@
+while true; do
+	read -p "New install? (y/n) " yn
+    case $yn in
+        [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr; 
+                git clone https://github.com/maxlttr/wm.git;
+                
+                #Polybar
+                mkdir ~/.config/polybar/;
+                cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+               
+                #Qtile
+                cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
+                chmod +x ~/wm/qtile/autostart.sh;
+                cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
+               
+                #Wallpaper
+                cp -r ~/wm/wallpapers/ ~/.config/qtile/wallpapers/;
+                
+                #Mouse
+                sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+                
+                #Configure TLP
+                alacritty -e sudo vim /etc/tlp.conf;
+	       	alacritty -e firefox -new-window https://linrunner.de/tlp/settings/introduction.html;
+
+                
+                #Clean useless file and folder
+                sudo rm -r ~/wm/ && rm installer.sh; 
+
+		read -p "Reboot now? (y/n) " yn
+                case $yn in
+                	[Yy]* ) reboot; break;;
+                	[Nn]* ) exit;;
+		esac
+		break;;
+
+	[Nn]* ) read -p "Do you wish to update? (y/n) ";
+		case $yn in
+			[Yy]* ) git clone https://github.com/maxlttr/wm.git;
+                        
+                                #Polybar
+                                mkdir ~/polybar/.config/polybar/;
+                                cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+                              
+                                #Qtile
+                                cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
+                                chmod +x ~/wm/qtile/autostart.sh;
+                                cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
+                               
+                                #Wallpaper
+                                cp -r ~/wm/wallpapers/ ~/.config/qtile/wallpapers/;
+                               
+                                #Mouse
+                                sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+                               
+                                #Clean useless file and folder
+                                sudo rm -r ~/wm/ && rm installer.sh;
+
+				read -p "Reboot now? (y/n) " yn;
+                                case $yn in
+                                        [Yy]* ) reboot; break;;
+                                        [Nn]* ) exit;;
+				esac
+			       	break;;
+
+			[Nn]* ) exit;;
+		esac
+    esac
+done
+
