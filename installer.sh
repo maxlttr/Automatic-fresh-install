0df96818e49089b4eabf3261d361477f4db4e04d
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-06-01 20:24:26 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,73 +1,33 @@
 while true; do
-    read -p "New install? (y/n) " yn
+    read -p "Install basic packages? (y/n) " yn
     case $yn in
-        [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr; 
+        [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr nitrogen; 
                 git clone https://github.com/maxlttr/wm.git;
-                
-                #Polybar
-                mkdir ~/.config/polybar/;
-                cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
-               
-                #Qtile
-                cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
-                chmod +x ~/wm/qtile/autostart.sh;
-                cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
-               
-                #Wallpaper
-                cp -r ~/wm/wallpapers/ ~/.config/qtile/;
-                
-                #Mouse
-                sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
-
-  		#sxhkd
-    		mkdir ~/.config/sxhkd/
-      		cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc		
-                
-                #Configure TLP
-		alacritty -e firefox -new-window https://linrunner.de/tlp/settings/introduction.html;
-                alacritty -e sudo vim /etc/tlp.conf;
-	
-                #Clean useless file and folder
-                sudo rm -r ~/wm/ && rm installer.sh; 
-
-		read -p "Reboot now? (y/n) " yn
-                case $yn in
-                	[Yy]* ) reboot; break;;
-                	[Nn]* ) exit;;
-		esac
-		break;;
-
-	[Nn]* ) read -p "Do you wish to update? (y/n) ";
-		case $yn in
-			[Yy]* ) git clone https://github.com/maxlttr/wm.git;
-                        
-                                #Polybar
-                                mkdir ~/polybar/.config/polybar/;
-                                cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
-                              
-                                #Qtile
-                                cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
-                                chmod +x ~/wm/qtile/autostart.sh;
-                                cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
-                               
-                                #Wallpaper
-                                cp -r ~/wm/wallpapers/ ~/.config/qtile/wallpapers/;
-                               
-                                #Mouse
-                                sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
-                               
-                                #Clean useless file and folder
-                                sudo rm -r ~/wm/ && rm installer.sh;
-
-				read -p "Reboot now? (y/n) " yn;
-                                case $yn in
-                                        [Yy]* ) reboot; break;;
-                                        [Nn]* ) exit;;
-				esac
-			       	break;;
-
-			[Nn]* ) exit;;
-		esac
+        [Nn]* ) exit;;
     esac
-done
+    echo "Beginning config installation..."
+    
+    #Polybar
+    mkdir ~/.config/polybar/;
+    cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+               
+    #Qtile
+    cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
+    chmod +x ~/wm/qtile/autostart.sh;
+    cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
+               
+    #Wallpaper
+    cp -r ~/wm/wallpapers/ ~/.config/qtile/;
+                
+    #Mouse
+    sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
 
+    #sxhkd
+    mkdir ~/.config/sxhkd/
+    cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc		
+                
+    #Clean useless file and folder
+    sudo rm -r ~/wm/ && rm installer.sh; 
+
+    echo "Don't forget to configure TLP before rebooting..."
+done                
