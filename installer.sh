a0fcf9ffbdc1628dabb84c79a1d525940f1a1f37
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-06-01 21:57:40 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,35 +1,42 @@
 read -p "Install basic packages? (y/n) " yn
-    case $yn in
-        [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr nitrogen; 
-                git clone https://github.com/maxlttr/wm.git;
-                exit;;
-        [Nn]* ) exit;;
-    esac
-    echo "Beginning config installation..."
+case $yn in
+    [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr nitrogen; 
+            git clone https://github.com/maxlttr/wm.git;
+            exit;;
+    [Nn]* ) exit;;
+esac
+echo "Beginning config installation..."
     
-    #Polybar
-    mkdir ~/.config/polybar/;
-    cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
-               
-    #Qtile
-    cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
-    chmod +x ~/wm/qtile/autostart.sh;
-    cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
-               
-    #Wallpaper
-    cp -r ~/wm/wallpapers/ ~/.config/;
-                
-    #Mouse
-    sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+#Qtile
+resolution=("1920x1080" "1366x768")
+select fav in "${resolution[@]}" do;
+case $fav in
+    "1920x1080") 
+        cp -i ~/wm/qtile/1920x1080/config.py ~/.config/qtile/config.py;
+        exit;;
+        
+    "1366x768") 
+        cp -i ~/wm/qtile/1366x768/config.py ~/.config/qtile/config.py;
+        exit;;
+esac
+chmod +x ~/wm/qtile/autostart.sh;
+cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
 
-    #sxhkd
-    mkdir ~/.config/sxhkd/
-    cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc		
+#Polybar
+mkdir ~/.config/polybar/;
+cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+               
+#Wallpaper
+cp -r ~/wm/wallpapers/ ~/.config/;
                 
-    #Clean useless file and folder
-    sudo rm -r ~/wm/ && rm installer.sh; 
+#Mouse
+sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
 
-    echo "Don't forget to configure TLP before rebooting..."
-done                
+#sxhkd
+mkdir ~/.config/sxhkd/
+cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc		
+                
+#Clean useless file and folder
+sudo rm -r ~/wm/ && rm installer.sh; 
 
 echo "Don't forget to configure TLP before rebooting..."
