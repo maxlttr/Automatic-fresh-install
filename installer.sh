944087f59b6685fd8a21ce0d5f42572760a88206
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-06-03 18:51:49 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,44 +1,63 @@
-read -p "Install basic packages? (y/n) " yn
-case $yn in
-    [Yy]* ) sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd firefox qtile polybar rofi tlp xorg-xrandr arandr nitrogen; 
-            git clone https://github.com/maxlttr/wm.git;
-            break;;
-    [Nn]* ) break;;
-esac
-echo "Beginning config installation..."
-git clone https://github.com/maxlttr/wm.git
-#Qtile
-resolution=("1920x1080" "1366x768")
-select fav in "${resolution[@]}" 
+echo "Please choose what you want to do"
+
+options=("New Install (includes basic packages)" "Update")
+select option in "$(options[@]}"
 do
-    case $fav in
-        "1920x1080") 
-            cp -i ~/wm/qtile/1920x1080/config.py ~/.config/qtile/config.py;
-            break;;
-        "1366x768") 
-            cp -i ~/wm/qtile/1366x768/config.py ~/.config/qtile/config.py;
-            break;;
+    case $option in
+        "New Install (includes basic packages)")
+            echo "Beginning config installation...";
+            sudo pacman -S git neofetch htop vim networkmanager brightnessctl otf-aurulent-nerd qtile polybar rofi tlp xorg-xrandr arandr nitrogen; 
+            git clone https://github.com/maxlttr/wm.git;
+            cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
+            chmod +x ~/wm/qtile/autostart.sh;
+            cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
+            #Polybar
+            mkdir ~/.config/polybar/;
+            cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+            #Wallpaper
+            cp -r ~/wm/wallpapers/ ~/.config/;
+            #Mouse
+            sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+            #sxhkd
+            mkdir ~/.config/sxhkd;
+            cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc;
+            cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc;
+            #Clean useless file and folder;
+            sudo rm -r ~/wm/ && rm installer.sh; 
+            echo "Don't forget to configure TLP before rebooting...";
+            read -p "Reboot now? (y/n) " yn;
+            case $yn in
+                [Yy]* ) reboot;
+                        break;;
+                [Nn]* ) break;;
+            esac
+            exit;;
+        "Update")
+            echo "Beginning config update...";
+            git clone https://github.com/maxlttr/wm.git;
+            cp -i ~/wm/qtile/config.py ~/.config/qtile/config.py;
+            chmod +x ~/wm/qtile/autostart.sh;
+            cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
+            #Polybar
+            mkdir ~/.config/polybar/;
+            cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
+            #Wallpaper
+            cp -r ~/wm/wallpapers/ ~/.config/;
+            #Mouse
+            sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+            #sxhkd
+            mkdir ~/.config/sxhkd;
+            cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc;
+            cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc;
+            #Clean useless file and folder;
+            sudo rm -r ~/wm/ && rm installer.sh; 
+            echo "Don't forget to configure TLP before rebooting...";
+            read -p "Reboot now? (y/n) " yn;
+            case $yn in
+                [Yy]* ) reboot;
+                        break;;
+                [Nn]* ) break;;
+            esac
+            exit;;
     esac
 done
-
-chmod +x ~/wm/qtile/autostart.sh;
-cp -i ~/wm/qtile/autostart.sh ~/.config/qtile/autostart.sh;
-
-#Polybar
-mkdir ~/.config/polybar/;
-cp -i ~/wm/polybar/config.ini ~/.config/polybar/config.ini;
-               
-#Wallpaper
-cp -r ~/wm/wallpapers/ ~/.config/;
-                
-#Mouse
-sudo cp -i ~/wm/mouse/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
-
-#sxhkd
-mkdir ~/.config/sxhkd/
-cp -i ~/wm/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc		
-                
-#Clean useless file and folder
-sudo rm -r ~/wm/ && rm installer.sh; 
-
-echo "Don't forget to configure TLP before rebooting..."
